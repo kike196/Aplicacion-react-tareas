195b814072d63d42441091de8dbabe8648356a43
earanaga <earanaga.fc@gmail.com>
2023-04-03 19:19:40 -0400

    metodo localStorage y hook useEffect

--- src/componentes/ListaDeTareas.js
+++ src/componentes/ListaDeTareas.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 import TareaFormulario from "./TareaFormulario";
 import Tarea from "./Tarea";
 import '../estilos/listaDeTareas.css';
@@ -7,27 +7,41 @@ function ListaDeTareas () {
 
   const [tareas, setTareas] = useState([]);
 
+  useEffect(() => {
+    const tareasGuardadas = JSON.parse(localStorage.getItem('tareas'));
+    if (tareasGuardadas) {
+      setTareas(tareasGuardadas);
+    } else {
+      setTareas([]);
+    }
+  }, []);
+
+  // La función localStorage es una API de almacenamiento en el navegador que permite guardar datos en el dispositivo del usuario. El método getItem() de localStorage permite leer un elemento específico almacenado en localStorage. 
+
   const agregarTarea = tarea => {
-    if (tarea.texto.trim()) {
-      tarea.texto = tarea.texto.trim();
-      const tareasActualizadas = [tarea, ...tareas];
-      setTareas(tareasActualizadas);
+    if (tarea.texto.trim()) {  // Verificar que la tarea no esté vacía
+      tarea.texto = tarea.texto.trim(); // Si hay texto en la tarea, quita espacios en blanco innecesarios
+      const tareasActualizadas = [tarea, ...tareas]; // Actualizar la lista de tareas agregando la nueva tarea al inicio
+      setTareas(tareasActualizadas); // Actualizar el estado de las tareas con la lista actualizada
+      localStorage.setItem('tareas', JSON.stringify(tareasActualizadas)); //guarda la tarea en localStorage
     }
   };
 
   const eliminarTarea = id => {
-    const tareasActualizadas = tareas.filter(tarea => tarea.id !==id);
-    setTareas(tareasActualizadas);
+    const tareasActualizadas = tareas.filter(tarea => tarea.id !==id); // Filtrar la lista de tareas para obtener todas las tareas excepto la que se va a eliminar
+    setTareas(tareasActualizadas); // Actualizar el estado de las tareas con la lista actualizada
+    localStorage.setItem('tareas', JSON.stringify(tareasActualizadas)); //elimina la tarea de localStorage
   };
 
   const completarTarea = id => {
-    const tareasActualizadas = tareas.map(tarea => {
-      if (tarea.id === id) {
+    const tareasActualizadas = tareas.map(tarea => { // Usar el método map para crear una nueva lista de tareas actualizada
+      if (tarea.id === id) { // Si la tarea actual es la que se está buscando, cambiar su estado de completada
         tarea.completada = !tarea.completada;
       }
       return tarea;
     })
-    setTareas(tareasActualizadas);
+    setTareas(tareasActualizadas); // Actualizar el estado de las tareas con la lista actualizada
+    localStorage.setItem('tareas', JSON.stringify(tareasActualizadas)); //busca la tarea en localStorage
   };
 
   return (
